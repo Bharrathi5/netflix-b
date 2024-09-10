986af4324dfa5da0ae7778c21de3ccecca45a3b0
Bharrathi5 <bharathimanogaran5@gmail.com>
2024-09-10 10:31:01 +0530

    removed unused code

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react'
-import openai from "../utils/openai.js"
 
 const SearchBar = () => {
     
@@ -27,7 +26,7 @@ const SearchBar = () => {
       <div className='flex justify-center'>
           <form  onSubmit ={(event) => event.preventDefault()} className='m-36 text-white'>
             <input ref={searchText} type='text' placeholder='Ask GPT for Movie Recommendations' className='p-3 m-3 w-96 bg-transparent border-2 rounded-lg'/>
-            <button type="button" onClick={handleSearch} className= "text-black font-semibold bg-slate-400 hover:bg-neutral-600  rounded-lg w-24 h-12">Search</button>
+            <button type="button" className= "text-black font-semibold bg-slate-400 hover:bg-neutral-600  rounded-lg w-24 h-12">Search</button>
           </form>  
       </div>
     )
